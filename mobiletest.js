5fed02dc444b2529df2be6a913c684bac8328cc7
GFruit <71150343+GFruit@users.noreply.github.com>
2020-10-22 12:12:45 +0200

    test

--- mobiletest.js
+++ mobiletest.js
@@ -5,5 +5,56 @@ if( navigator.userAgent.match(/Android/i)
  || navigator.userAgent.match(/iPod/i)
  || navigator.userAgent.match(/BlackBerry/i)
  || navigator.userAgent.match(/Windows Phone/i)) {
-    alert("You're using Mobile Device!!")
+    document.getElementById('input').addEventListener("keypress", getValue);
+ } else {
+    document.getElementById('input').addEventListener("keydown", getValue);
  }
+
+obj = {
+    i: 0,
+    lettercount: 0
+}
+
+function getValue() {
+    if (obj.i-1 > obj.lettercount) {
+        return;
+    }
+    input = document.getElementById('input').value;
+    document.getElementById('result').innerHTML = input;
+    len = input.length;
+    text = document.querySelector('letter').innerHTML;
+    letter = document.querySelectorAll('letter')[obj.i];
+    if (input[obj.i] == letter.innerHTML) {
+        letter.style.color = "green";
+    } else {
+        letter.style.color = "red";
+    }
+    obj.i++;
+}
+
+wordlist = ['hello', 'my', 'friend']
+for (word of wordlist) {
+    for (letter of word) {
+        obj.lettercount++;
+    }
+}
+
+text = document.getElementById('text');
+
+for (var i=0; i<wordlist.length; i++) {
+    wordTag = document.createElement('word');
+    for (let x=0; x<wordlist[i].length; x++) {
+        letterTag = document.createElement('letter');
+        letter = document.createTextNode(wordlist[i][x]);
+        letterTag.appendChild(letter);
+        wordTag.appendChild(letterTag);
+    }
+    text.appendChild(wordTag);
+    letterTag = document.createElement('letter');
+    letter = document.createTextNode(' ');
+    letterTag.appendChild(letter);
+    text.appendChild(letterTag);
+}
+lastspace = text.lastChild;
+text.removeChild(lastspace);
+
