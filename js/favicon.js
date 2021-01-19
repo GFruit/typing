e106b93edfa38d27d4fabb220711e9c10662a9ce
GFruit <71150343+GFruit@users.noreply.github.com>
2021-01-20 00:31:04 +0100

    favicon bug fix attempt

--- js/favicon.js
+++ js/favicon.js
@@ -1,8 +1,11 @@
 let link = document.querySelector("link[rel~='icon']")
-let faviconUrl = link.href;
+let faviconUrl = "https://gfruit.github.io/typing/keyboard.ico" //link.href;
 let img = document.createElement("img");
 img.addEventListener("load", onImageLoaded);
+img.crossOrigin = "anonymous";
 img.src = faviconUrl;
+
+console.log(img.src)
 
 function onImageLoaded() {
     subColor = getComputedStyle(document.querySelector(':root')).getPropertyValue("--sub-color");
